780b3fb8795f5472cd8b6a6f7b589f4b02d45aa2
Rupa Acharya <rupaacharya87@gmail.com>
2021-03-25 22:00:43 -0500

    Learn.js file commited

---
+++ Learn.js
@@ -0,0 +1,16 @@
+let getRandomNumber = function(start , range)
+{
+    let getRandom = Math.floor((Math.random()*range)+start);
+    return getRandom;
+}
+console.log(getRandomNumber(0,7));
+let userName = "Rupa";
+userName ? console.log(`Hello, ${userName}`): console.log("Hello!");
+let userQuestion = " Can you answer my question? ";
+console.log(`The user asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random()*8);
+
+
+
+
+
